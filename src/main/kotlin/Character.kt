f929dc394caeb0ff11d12214d2d20fee695736ae
Engelbertus Hendro <engelbertushendro10@gmail.com>
2020-11-27 01:38:28 +0700

    fix codingan

--- src/main/kotlin/Character.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-//    character nggak boleh lebih dari satu huruf
-    var char: Char = 'e'
-    var a:Char = 'k'
-
-    print(char)
-    print(a)
-    
-}
--- src/main/kotlin/Constant.kt
+++
@@ -1,9 +0,0 @@
-// Const wajib pake val
-// digunakan untuk global variable
-
-const val APP = "Kotlin Dasar"
-const val VERSION = "0.0.1"
-
-fun main() {
-    println("WELCOME TO $APP version $VERSION")
-}
